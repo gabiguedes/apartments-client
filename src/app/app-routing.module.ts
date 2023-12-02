2aa8f63e1a2233ad4cc05fbe4f2bc279202054df
gabiguedes <zooiv3rde@gmail.com>
2023-12-02 19:43:26 -0300

    feat: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from "./components/authentication/authentication.component";
 import { UserNoobComponent } from "./components/user-noob/user-noob.component";
+import {AdminSupremeComponent} from "./components/admin-supreme/admin-supreme.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/pagina-inicial', pathMatch: 'full' },
   { path: 'pagina-inicial', component: AuthenticationComponent },
   { path: 'user-noob', component: UserNoobComponent },
-  // ... outras rotas
+  { path: 'admin-supreme', component: AdminSupremeComponent },
 ];
 
 @NgModule({
